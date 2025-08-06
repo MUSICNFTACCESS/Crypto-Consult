62de7b642be0ff16bae362d90b1a91aa0ff7b38a
MUSICNFTACCESS <crimzncipriano@gmail.com>
2025-08-05 22:32:43 -0400

    🧹 Temp: Remove wildcard route to test index.html serving

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ try {
 
 // ⚙️ App Init
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 3000;
 
 // 🔓 Usage Tracking
@@ -249,7 +250,6 @@ app.get("/livePrices", async (req, res) => {
   }
 });
 
-// 🌐 Wildcard Route (put this last!)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
